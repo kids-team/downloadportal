4e99e7bde4d4208593ce27589f50e785f542003f
Thomas Gollenia <thomas@gollenia.at>
2022-10-07 11:36:18 +0200

    Bible redone

--- src/routes/Bible.tsx
+++ src/routes/Bible.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Combobox from '../components/Combobox';
+import Header from '../components/Header';
 import Navigation from '../components/Navigation';
+import NumberSelect from '../components/NumberSelect';
 import { Verse } from '../services/models/bible';
 import { store } from '../services/store';
 import useFetch from '../services/useFetch';
@@ -11,45 +14,72 @@ const Bible = () => {
 	const { book, chapter } = params;
 	const { state } = useContext(store)
 
-	const bookId = state.bible.books.find(item => item.short_name === book) ?? 10;
-	
+
+	const currentBook = state.bible.books.find((item) => { return item.short_name === book });
+
+	const navigate = useNavigate();
+
 	const args = {
 		controller: 'bible',
 		lang: state.lang,
-		book: bookId,
-		chapter
+		book: currentBook?.id ?? 10,
+		chapter: chapter ?? 1
+	}
+	console.log(book, currentBook)
+	const url = useUrl(args);
+	const { data, error } = useFetch<Array<Verse>>(url);
+
+
+	const tagUrl = useUrl({
+		controller: 'tag',
+		tag: currentBook?.short_name
+	});
+
+	const { data: tag, error: tagError } = useFetch(tagUrl);
+	console.log(tag)
+
+	const bibleurl = state.bible?.info?.url ?? "bible"
+	const bookOptions = state.bible.books.map((book) => { return { label: book.long_name, value: book.short_name } })
+
+	const setBook = (bookId: number | string) => {
+		navigate(`/${bibleurl}/${bookId}/1`);
 	}
 
-	const url = useUrl(args);
-	const {data, error} = useFetch<Array<Verse>>(url);	  
+	const setChapter = (chapter: number) => {
+		navigate(`/${bibleurl}/${currentBook?.short_name ?? 'genesis'}/${chapter}`);
+	}
+
 
 	return (
 		<>
-		<Navigation />
-		<header className="header" style={{paddingTop: '20vh'}}>
-			
-			<div className="content">
-				<div className="header__title">
-					<h1 className="font-script">{state.bible.name}</h1>
-					<h4 className="ml-4 xl:mr-0">{chapter}</h4>
+			<Navigation />
+			<Header title={currentBook?.long_name} subtitle={state.bible.info.chapter_string + ' ' + chapter ?? ''} />
+			<div className='section bg-gray-300 py-12'>
+				<div className='content flex gap'>
+					<Combobox
+						options={bookOptions}
+						onChange={setBook}
+						placeholder={currentBook?.long_name}
+					/>
+					<NumberSelect max={currentBook?.chapters ?? 1} min={1} onChange={setChapter} />
 				</div>
 			</div>
-			
-		</header>
-		<div className='section bg-gray-300 py-12'>
-			<div className='content'>
-				CONTROLS
-			</div>
-		</div>
-		<section>
-			<div className='container'>
-			<ul>
-				{ data?.map((verse) => {
-					return <li><sup>{verse.verse}</sup>{verse.text}</li>
-				}) }
-			</ul>
-			</div>
-		</section>
+			<section>
+				<div className='container grid grid--columns-4 grid--gap-4'>
+					<div className='sm:grid__column--span-4 lg:grid__column--span-3'>
+						<div>
+							<ul>
+								{data?.map((verse) => {
+									return <li><sup>{verse.verse}</sup>{verse.text}</li>
+								})}
+							</ul>
+						</div>
+					</div>
+					<div>
+						<h4>Artikel</h4>
+					</div>
+				</div>
+			</section>
 		</>
 	)
 }
