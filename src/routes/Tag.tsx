f955556a829f1efb4323156be8f2eed2fab82a81
Thomas Gollenia <thomas@gollenia.at>
2022-10-12 13:41:09 +0200

    Header

--- src/routes/Tag.tsx
+++ src/routes/Tag.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import Card from '../components/Card';
+import Header from "../components/Header";
 import Navigation from '../components/Navigation';
 import { Article } from '../services/models/article';
 import useFetch from '../services/useFetch';
@@ -14,46 +15,41 @@ const Tag = () => {
 
 	const params = useParams();
 	let tagName = params.tag;
-	
+
 	const args = {
 		controller: 'pages',
 		method: 'get_tag',
 		tag: tagName,
 	}
-	
+
 	const url = useUrl(args);
 	const { data, error } = useFetch<TagResult>(url)
-	  
-	
+
+
 	return (
 		<>
-		<Navigation />
-		<header className="header" style={{paddingTop: '30vh'}}>
-			<div className="content">
-				<div className="header__title">
-					<h1 className="font-script">{data?.root?.title}</h1>
-					<h4 className="ml-4 xl:mr-0">Tag</h4>
+			<Navigation />
+			<Header
+				title={data?.root.title}
+				image={data?.root.pageimage}
+			/>
+			<section className="bg-gray-100 py-12">
+				<div className='content'>
+					<div className='grid grid--columns-1 md:grid--columns-2 xl:grid--columns-3 grid--gap-12 mt-12'>
+						{data?.articles.map((article, index) => {
+							return <Card
+								key={index}
+								title={article.title}
+								label={article.category}
+								text={article.abstract}
+								image={article.pageimage}
+								link={article.id}
+							/>
+						})}
+					</div>
 				</div>
-			</div>
-		</header>
-		<section className="bg-gray-100 py-12">
-			<div className='content'>
-				<div className='grid grid--columns-1 md:grid--columns-2 xl:grid--columns-3 grid--gap-12 mt-12'>
-				{ data?.articles.map((article, index) => {
-					return <Card 
-					key={index}
-						title={article.title}
-						label={article.category}
-						text={article.abstract}
-						image={article.image?.src}
-						link={article.id}
-		
-					/>
-				}) }
-				</div>
-			</div>
-		</section>
-		
+			</section>
+
 		</>
 	)
 }
