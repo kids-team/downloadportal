ca028f9fe1b861890f42b4cb4c30e87325074fc2
Thomas Gollenia <thomas@gollenia.at>
2022-10-14 07:18:11 +0200

    router now external

---
+++ src/services/routes.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom"
+import ArticlePage from "../routes/ArticlePage"
+import Bible from "../routes/Bible"
+import SearchPage from "../routes/SearchPage"
+import Tag from "../routes/Tag"
+//somehow we need to reintegrate the state
+//const bibleurl = state.bible?.info?.url ?? "bible"
+const router = createBrowserRouter([
+	{
+		element: <Bible />,
+		path: "bible/:book/:chapter"
+	},
+	{
+		element: <SearchPage />,
+		path: "search/:query",
+	},
+	{
+		element: <Tag />,
+		path: "tag/:tag"
+	},
+	{
+		element: <ArticlePage />,
+		path: "*"
+	}
+])
+
+export default router;
