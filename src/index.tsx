fa9ca3cf13a8c4f9b851a8ebc97ee77702a58a5c
Thomas Gollenia <thomas@gollenia.at>
2022-10-28 08:04:24 +0200

    new test build

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './services/store';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-	<StateProvider>
-    <App />
-	</StateProvider>
-  </React.StrictMode>
+	<>
+		<StateProvider>
+			<App />
+		</StateProvider>
+	</>
 );
 
 // If you want to start measuring performance in your app, pass a function
