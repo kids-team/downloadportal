87deb0d1e281374a2b653a2d1134fcc6adc6bd17
Thomas Gollenia <thomas@gollenia.at>
2022-12-13 13:12:09 +0100

    header__contetn

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,46 +2,49 @@ import React from 'react';
 import { useMediaUrl } from '../../services/useUrl';
 
 export interface Props {
-	title?: string;
-	subtitle?: string;
-	image?: string;
-	minimal?: boolean;
-	aspectRatio?: "21" | "16" | "12";
+    title?: string;
+    subtitle?: string;
+    image?: string;
+    minimal?: boolean;
+    aspectRatio?: '21' | '16' | '12';
 }
 
 const Header: React.FC<Props> = props => {
+    const { image, title, subtitle, aspectRatio, minimal } = props;
 
-	const { image, title, subtitle, aspectRatio, minimal } = props;
+    const imageSrc: string = useMediaUrl(image, 1440);
 
-	const imageSrc: string = useMediaUrl(image, 1440);
+    const className: string = [
+        'header',
+        minimal ? 'header--minimal' : 'aspect-12/9 lg:aspect-' + aspectRatio + '/9',
+        'header--left',
+        'header--bottom',
+        'fill-body',
+    ]
+        .filter(Boolean)
+        .join(' ');
 
-	const className: string = [
-		'header',
-		minimal ? 'header--minimal' : 'aspect-12/9 lg:aspect-' + aspectRatio + '/9',
-		'header--left',
-		'header--bottom',
-		'fill-body'
-	].filter(Boolean).join(' ')
+    return (
+        <header className={className}>
+            {image && <img src={imageSrc} />}
 
-	return (
-		<header className={className}>
-			{image && <img src={imageSrc} />}
-
-
-			<div className="header__title">
-				<h1 className="font-script">{title}</h1>
-				{subtitle && <h4 className="ml-4 xl:mr-0">{subtitle}</h4>}
-			</div>
-
-		</header>
-	)
-}
-
-Header.defaultProps = {
-	aspectRatio: "16",
-	image: "",
-	title: "",
-	minimal: false
+            <div className="header__content">
+                <div>
+                    <div className="header__title">
+                        <h1 className="font-script">{title}</h1>
+                        {subtitle && <h4 className="ml-4 xl:mr-0">{subtitle}</h4>}
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
-export default Header
+Header.defaultProps = {
+    aspectRatio: '16',
+    image: '',
+    title: '',
+    minimal: false,
+};
+
+export default Header;
