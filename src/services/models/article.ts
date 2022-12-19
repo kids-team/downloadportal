b3bbcb588f8e0026552df33206f41bb0581aee59
Thomas Gollenia <thomas@gollenia.at>
2022-12-19 14:53:38 +0100

    added card label

--- src/services/models/article.ts
+++ src/services/models/article.ts
@@ -1,61 +1,62 @@
-import { BibleRef } from "./bible";
-import PhpDate from "./date";
+import { BibleRef } from './bible';
+import PhpDate from './date';
 
 export type Article = {
-	id: string;
-	title: string;
-	content: string;
-	abstract: string;
-	namespace: string;
+    id: string;
+    title: string;
+    content: string;
+    abstract: string;
+    namespace: string;
+    label: string;
 
-	minor_change: boolean;
-	user: string;
-	date: PhpDate;
-	created: PhpDate;
+    minor_change: boolean;
+    user: string;
+    date: PhpDate;
+    created: PhpDate;
 
-	category: string;
-	audience: string;
-	tags: Array<string>;
-	bibleref: Array<BibleRef>;
+    category: string;
+    audience: string;
+    tags: Array<string>;
+    bibleref: Array<BibleRef>;
 
-	icon: string;
-	files: Array<DokuFile>;
-	pagelink: string;
-	pageimage: string;
-	copyright: string;
+    icon: string;
+    files: Array<DokuFile>;
+    pagelink: string;
+    pageimage: string;
+    copyright: string;
 
-	showSubpages: boolean;
-}
+    showSubpages: boolean;
+};
 
 export type DokuFile = {
-	id: string;
-	path: string;
-	thumbnail: string;
-	src: string;
-	full: string;
-	filename: string;
-	size: number;
-	modified: PhpDate;
-	count: number;
-	extension: string;
-	info: string;
-	writable: boolean
-	minor_change: boolean;
-}
+    id: string;
+    path: string;
+    thumbnail: string;
+    src: string;
+    full: string;
+    filename: string;
+    size: number;
+    modified: PhpDate;
+    count: number;
+    extension: string;
+    info: string;
+    writable: boolean;
+    minor_change: boolean;
+};
 
 export type Tag = {
-	name: string,
-	id: string
-}
+    name: string;
+    id: string;
+};
 
 export type Category = {
-	value: string
-	label: string
-	icon: string,
-	color: string
-}
+    value: string;
+    label: string;
+    icon: string;
+    color: string;
+};
 
 export type Audience = {
-	value: string
-	label: string
-}
+    value: string;
+    label: string;
+};
