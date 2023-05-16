c507760e022158b64af7ae273e7d49a3b7ea5511
Thomas Gollenia <thomas@gollenia.at>
2023-05-16 10:30:41 +0200

    beautify

--- src/services/getTopLevelDomain.ts
+++ src/services/getTopLevelDomain.ts
@@ -1,15 +1,14 @@
 /*
-*	gets the Top level domain and cares about a possible port number
-*
-*/
+ *	gets the Top level domain and cares about a possible port number
+ *
+ */
 function getTopLevelDomain(fallback: string = 'de'): string {
-	if(!window.location.origin.includes('.')) return fallback;
-	let tld: string = window.location.origin.split('.').pop() ?? fallback;
-	console.log(tld)
-	if(tld.includes(':')) {
-		tld = tld.split(':')[0];
-	}
-	return tld;
+    if (!window.location.origin.includes('.')) return fallback;
+    let tld: string = window.location.origin.split('.').pop() ?? fallback;
+    if (tld.includes(':')) {
+        return tld.split(':')[0];
+    }
+    return tld;
 }
 
 export default getTopLevelDomain;
